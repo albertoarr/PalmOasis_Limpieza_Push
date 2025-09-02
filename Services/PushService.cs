c91e1cce3a48692795eb0712b0a3c59d7fbaea91
albertoarr <albertoarranzromano@gmail.com>
2025-09-02 09:33:51 +0100

    funcional

--- Services/PushService.cs
+++ Services/PushService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Threading;
-using System.Collections.Generic;
-using Microsoft.Extensions.Configuration;
-using FirebaseAdmin;
+﻿using FirebaseAdmin;
 using FirebaseAdmin.Messaging;
 using Google.Apis.Auth.OAuth2;
 using PalmOasis_Limpieza_Push.Models.Interfaces;
